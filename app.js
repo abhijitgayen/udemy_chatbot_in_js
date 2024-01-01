12492e81db8a9edff0907df8bf49fe707e8ce2ad
Abhijit Gayen <abhijit@peartree.to>
2024-01-01 17:39:12 +0530

    remove files

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.use(express.json());
 
 
 const {botResponse} = require('./chatbot/botResponse')
-const {sendMessage} = require('./whatsapp/sendMessage.js')
-const {sendCommunication} = require('./whatsapp/communication.js')
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -40,66 +38,8 @@ app.post('/bot_response', (req, res) =>{
 })
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+    console.log('Server started on port 3000');
 });
-
-app.post('/send_template_message', async(req,res)=>{
-    const coming_body = req.body
-    if (coming_body?.user_mobile_no){
-        let user_mobile_no =coming_body.user_mobile_no
-        let response = await sendCommunication(phone_no_id,token,user_mobile_no)
-        res.send(response)
-    }
-    else{
-        res.send('send me correct paload message')
-    }    
-})
-
-// whatsapp webhooks
-app.get('/webhooks', (req, res) => {
-    let mode = req.query["hub.mode"];
-    let challange = req.query["hub.challenge"];
-    let token = req.query["hub.verify_token"];
-
-    if (mode && token) {
-        if (mode === "subscribe" && token === myToken) {
-          res.status(200).send(challange);
-        } else {
-          res.status(403);
-        }
-    }
-})
-// get messsage from whatsapp
-app.post('/webhooks', (req, res) => {
-    let body_param = req.body;
-
-    if (body_param?.object){
-        if (body_param?.entry && body_param?.entry[0]?.changes && body_param?.entry[0]?.changes[0]?.value?.messages){
-            let from = body_param.entry[0].changes[0].value.messages[0].from;
-            let msg_id=body_param.entry[0].changes[0].value.messages[0].id;
-            let reply_type= body_param.entry[0].changes[0].value.messages[0].type;
-
-            if(reply_type==='text'){
-                let user_message = body_param.entry[0].changes[0].value.messages[0].text.body;
-                botResponse(user_message).then((response) =>{
-                    var message_to_send = {
-                        'message_type':'text',
-                        'message_data': {
-                            'body' : response.message
-                        }
-                    }
-                    console.log(user_message,'this is user message');
-                    sendMessage(phone_no_id,from,token,message_to_send)
-                });
-            }
-        }
-        res.sendStatus(200);
-    }
-    else{
-        res.sendStatus(404);
-    }
-
-})
 
 
 
